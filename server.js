a6ad7ca2d4d5a3ae1c8d12a58753e6d4871e953c
Josue Peralta <jperal77@gmail.com>
2018-08-31 12:11:53 -0500

    implemented get requests for actions and projects

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+projectDB = require('./data/helpers/projectModel.js');
+actionsDB = require('./data/helpers/actionModel.js');
 
 const server = express();
 
@@ -6,6 +8,24 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('Hello FSW12');
+});
+
+server.get('/api/projects', (req, res) => {
+    projectDB.get().then(projects => {
+        res.status(200).json(projects);
+    }).catch(err => {
+        console.error('Error', err);
+        res.status(500).json({message: "Error retrieving projects."});
+    });
+});
+
+server.get('/api/actions', (req, res) => {
+    actionsDB.get().then(actions => {
+        res.status(200).json(actions);
+    }).catch(err => {
+        console.error('Error', err);
+        res.status(500).json({message: "Error retrieving actions."});
+    });
 });
 
 server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
