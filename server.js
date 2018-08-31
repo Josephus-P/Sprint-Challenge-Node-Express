a85ca2fa6c1ffc91e65d040c4d3b2387de7765b0
Josue Peralta <jperal77@gmail.com>
2018-08-31 12:19:53 -0500

    implemented post handlers for actions and projects

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ server.get('/', (req, res) => {
     res.send('Hello FSW12');
 });
 
+server.post('/api/projects', (req, res) => {
+    
+    projectDB.insert(req.body).then(response => {
+        res.status(201).json(response);
+    }).catch(err => {
+        console.error('Error', err);
+        res.status(500).json({message: "Error creating project."});
+    });
+});
+
+server.post('/api/actions', (req, res) => {
+    
+    actionsDB.insert(req.body).then(response => {
+        res.status(201).json(response);
+    }).catch(err => {
+        console.error('Error', err);
+        res.status(500).json({message: "Error creating action."});
+    });
+});
+
 server.get('/api/projects', (req, res) => {
     projectDB.get().then(projects => {
         res.status(200).json(projects);
@@ -27,5 +47,6 @@ server.get('/api/actions', (req, res) => {
         res.status(500).json({message: "Error retrieving actions."});
     });
 });
+
 
 server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
