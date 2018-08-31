0be1bdc714d7294f57139dc8603c4c1d0aa704ee
Josue Peralta <jperal77@gmail.com>
2018-08-31 12:28:35 -0500

    implemented put and delete handlers for actions and projects

--- server.js
+++ server.js
@@ -48,5 +48,50 @@ server.get('/api/actions', (req, res) => {
     });
 });
 
+server.put('/api/projects/:id', (req, res) => {
+    projectDB.update(req.params.id, req.body).then(response => {
+        res.status(200).json(response);
+    })
+    .catch(err => res.status(500).json({ message: 'project update failed' }));
+});
+
+server.put('/api/actions/:id', (req, res) => {
+    actionDB.update(req.params.id, req.body).then(response => {
+        res.status(200).json(response);
+    })
+    .catch(err => res.status(500).json({ message: 'action update failed' }));
+});
+
+server.delete('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    projectDB.remove(id)
+        .then(response => {
+            console.log(response);
+            if(response) {
+                res.status(204).end();
+            }
+            else {
+                res.status(404).json({ message: 'No project with this id found'});
+            }
+            res.status(204).end();
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+server.delete('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    actionDB.remove(id)
+        .then(response => {
+            console.log(response);
+            if(response) {
+                res.status(204).end();
+            }
+            else {
+                res.status(404).json({ message: 'No action with this id found'});
+            }
+            res.status(204).end();
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
